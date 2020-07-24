ccfac67892fbea1ea996e601ded2ba2747a150ce
RyuichiroAbe <66513839+RyuichiroAbe@users.noreply.github.com>
2020-07-24 20:30:55 +0900

    listの編集、削除機能実装

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,5 +1,7 @@
 class ListController < ApplicationController
 
+  before_action :set_list, only: %i(edit update destroy)
+
   def new
     @list = List.new
   end
@@ -7,14 +9,34 @@ class ListController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to :root, notice: 'LISTが作成されました'
+      redirect_to :root
     else  
-      render action: :new, notice: 'LIST作成に失敗しました'
+      render action: :new
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @list.update_attributes(list_params)
+      redirect_to :root
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to :root
   end
 
   private
     def list_params
       params.require(:list).permit(:title).merge(user: current_user)
+    end
+
+    def set_list
+      @list = List.find_by(id: params[:id])
     end
 end
