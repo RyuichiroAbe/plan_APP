4c3f2578b6936c72a0d4ab2ad437fec0cbc84fb9
RyuichiroAbe <66513839+RyuichiroAbe@users.noreply.github.com>
2020-07-25 17:36:54 +0900

    機能　完成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
 
-  resources :list, only: [:new, :create, :edit, :update, :destroy]
+  resources :list, only: [:new, :create, :edit, :update, :destroy] do
+    resources :card, except: [:index]
+  end
 end
